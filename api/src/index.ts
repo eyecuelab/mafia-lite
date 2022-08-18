7a9832702cb5c9437ea005b5143eeb58cfee3fd7
William Rodriguez <william@rodriguez1.com>
2022-08-18 15:26:16 -0700

    Add game GET and POST endpoints

--- api/src/index.ts
+++ api/src/index.ts
@@ -9,148 +9,45 @@ app.use(cors({ origin: '*'}));
 
 app.use(express.json())
 
-app.post(`/signup`, async (req, res) => {
-  const { name, email, posts } = req.body
+app.post("/game", async (req, res) => {
+	const { hostId } = req.body;
+	if (!hostId) {
+		return res.status(500).json({ error: "Host required to start game"});
+	}
 
-  if (!email) {
-    return res.status(500).json({ error: `Email is required for signup.` })
-  }
+	const host = await prisma.user.findUnique({
+		where: { id: Number(hostId) },
+	});
+	if (!host) {
+		return res.status(500).json({ error: "Host not found"});
+	}
 
-  const postData = posts?.map((post: Prisma.PostCreateInput) => {
-    return { title: post?.title, content: post?.content }
-  })
+	const game = await prisma.game.create({
+		data: {
+			hostId
+		}
+	});
 
-  const result = await prisma.user.create({
-    data: {
-      name,
-      email,
-      posts: {
-        create: postData,
-      },
-    },
-  })
-  res.json(result)
+	res.json(game);
 })
 
-app.post(`/post`, async (req, res) => {
-  const { title, content, authorEmail } = req.body
-  const result = await prisma.post.create({
-    data: {
-      title,
-      content,
-      author: { connect: { email: authorEmail } },
-    },
-  })
-
-  res.json(result)
+app.get("/game", async (req, res) => {
+	const games = await prisma.game.findMany();
+	res.json(games);
 })
 
-app.put('/post/:id/views', async (req, res) => {
-  const { id } = req.params
+app.get("/game/:id", async (req, res) => {
+	const { id } = req.params;
 
-  try {
-    const post = await prisma.post.update({
-      where: { id: Number(id) },
-      data: {
-        viewCount: {
-          increment: 1,
-        },
-      },
-    })
+	try {
+		const game = await prisma.game.findUniqueOrThrow({
+			where: { id: Number(id) },
+		});
 
-    res.json(post)
-  } catch (error) {
-    res.json({ error: `Post with ID ${id} does not exist in the database` })
-  }
-})
-
-app.put('/publish/:id', async (req, res) => {
-  const { id } = req.params
-
-  try {
-    const postData = await prisma.post.findUnique({
-      where: { id: Number(id) },
-      select: {
-        published: true,
-      },
-    })
-
-    const updatedPost = await prisma.post.update({
-      where: { id: Number(id) || undefined },
-      data: { published: !postData?.published },
-    })
-    res.json(updatedPost)
-  } catch (error) {
-    res.json({ error: `Post with ID ${id} does not exist in the database` })
-  }
-})
-
-app.delete(`/post/:id`, async (req, res) => {
-  const { id } = req.params
-  const post = await prisma.post.delete({
-    where: {
-      id: Number(id),
-    },
-  })
-  res.json(post)
-})
-
-app.get('/users', async (req, res) => {
-  const users = await prisma.user.findMany()
-  res.json(users)
-})
-
-app.get('/user/:id/drafts', async (req, res) => {
-  const { id } = req.params
-
-  const drafts = await prisma.user
-    .findUnique({
-      where: {
-        id: Number(id),
-      },
-    })
-    .posts({
-      where: { published: false },
-    })
-
-  res.json(drafts)
-})
-
-app.get(`/post/:id`, async (req, res) => {
-  const { id }: { id?: string } = req.params
-
-  const post = await prisma.post.findUnique({
-    where: { id: Number(id) },
-  })
-  res.json(post)
-})
-
-app.get('/feed', async (req, res) => {
-  const { searchString, skip, take, orderBy } = req.query
-
-  const or: Prisma.PostWhereInput = searchString
-    ? {
-        OR: [
-          { title: { contains: searchString as string } },
-          { content: { contains: searchString as string } },
-        ],
-      }
-    : {}
-
-  const posts = await prisma.post.findMany({
-    where: {
-      published: true,
-      ...or,
-    },
-    include: { author: true },
-    take: Number(take) || undefined,
-    skip: Number(skip) || undefined,
-    orderBy: {
-      updatedAt: orderBy as Prisma.SortOrder,
-    },
-  })
-
-  res.json(posts)
+		res.json(game);
+	}  catch (error) {
+		return res.status(404).json({ error: "Game not found" });
+	}
 })
 
 const server = app.listen(3000, () =>
