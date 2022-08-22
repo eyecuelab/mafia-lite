6e7b8ae4afc082bbc6097571c4d1117c6b371a88
Marcus <marcus.kaitara.lorenzo@gmail.com>
2022-08-22 13:09:06 -0700

    Mount socket.io and resolve conflicts between schema changes, updated routes/controllers

--- api/src/Routes/game.ts
+++ api/src/Routes/game.ts
@@ -9,13 +9,4 @@ router.get("/game/:id", gameControllers.getSingleGame);
 router.post('/game', gameControllers.createGame);
 
 
-/* Test route for websockets using users data*/
-// import { PrismaClient } from '@prisma/client';
-// const prisma = new PrismaClient();
-// router.get('/users', async (req, res) => {
-//   const users = await prisma.user.findMany()
-//   res.json(users)
-// })
-/* Remove before pushing to dev branch */
-
 export default router;
