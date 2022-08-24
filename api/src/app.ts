b9f8c264738981fba573f59ad131ad9b00b54947
Mark McConnell <amark.mcconnell@gmail.com>
2022-08-23 19:39:27 -0700

    sessions integrated and role crud added

--- api/src/app.ts
+++ api/src/app.ts
@@ -5,18 +5,25 @@ import gameRouter from "./Routes/game";
 import roundRouter from "./Routes/round";
 import userRouter from "./Routes/user";
 import playerRouter from "./Routes/player";
+import roleRouter from "./Routes/role";
 
 
-const prisma = new PrismaClient()
-const app = express()
+const prisma = new PrismaClient();
+const app = express();
+const session = require('express-session');
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { }
+}))
 
 app.use(cors({ origin: '*'}));
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-
-app.use(gameRouter, userRouter, roundRouter, playerRouter)
-
-
+app.use(gameRouter, userRouter, roundRouter, playerRouter, roleRouter);
 
 export default app;
