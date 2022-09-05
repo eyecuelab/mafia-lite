648f11fd9a81b277576afa2132184ea9cacef8fa
Mark McConnell <amark.mcconnell@gmail.com>
2022-09-05 11:48:13 -0700

    create modalcontext file

---
+++ client/src/ModalContext.tsx
@@ -0,0 +1,19 @@
+import { createContext, ReactNode, useContext } from "react";
+
+// interface for the modal context
+export type ModalContextType = {
+  content: ReactNode | null;
+  isOpen: boolean;
+  callModal: (content: ReactNode) => void,
+  closeModal: () => void,
+}
+
+// create context sets the default value to null
+export const ModalContext = createContext<ModalContextType>({
+	content: null,
+	isOpen: false,
+	callModal: () => null,
+	closeModal: () => null,
+});
+
+export const useModal = () => useContext(ModalContext);
