a913b75b0d5fef837f5e9db1bb9018c5d581af4d
Elly <eliranmaimon@gmail.com>
2022-09-07 16:40:39 -0700

    update cors setting

--- api/src/app.ts
+++ api/src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors({ origin: [
   'http://localhost:5173',
-  'https://nameless-terror-client.fly.dev/'
+  'https://nameless-terror-client.fly.dev'
 ], credentials: true }));
 
 app.use(
