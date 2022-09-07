0487b5ea1ac87f9abe7c18e7331e7401e883ed34
Elly <eliranmaimon@gmail.com>
2022-09-07 15:15:28 -0700

    add deployed client url to cors origins

--- api/src/app.ts
+++ api/src/app.ts
@@ -9,7 +9,10 @@ const session = require("express-session")
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: [
+  'http://localhost:5173',
+  'https://nameless-terror-client.fly.dev/'
+], credentials: true }));
 
 app.use(
   session({
