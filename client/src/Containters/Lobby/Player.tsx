bb31f352256e12e0cde651e880c59162d09f15e9
William Rodriguez <william@rodriguez1.com>
2022-09-13 22:03:42 -0700

    Change cultist vote count to red

--- client/src/Containters/Lobby/Player.tsx
+++ client/src/Containters/Lobby/Player.tsx
@@ -49,7 +49,7 @@ const PlayerCardWrapper: React.FC<PlayerProps> = ({ player, isLobby, handleCastV
 					<PlayerCard player={player} isLobby={isLobby} playerStatus={"accused"} /> : */}
 				<PlayerCard player={player} playerStatus={player.status === "alive" ? playerStatus : player.status} isLobby={isLobby} team={team} canVote={canVote} phase={phase}/>
 				{(canVote) ? (!!numberOfVotes && (
-					<h5 className={styles.voteCounter}>Votes: {numberOfVotes}</h5>
+					<h5 className={`${styles.voteCounter} ${phase === "day" ? styles.dayVoteCounter : styles.nightVoteCounter}`}>Votes: {numberOfVotes}</h5>
 				)) : null }
 			</div>
 		</>
